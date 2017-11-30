c7b4a2ead278f6eaa58495cab499fbe6c5a2584b
Peter Robitaille <peterrobitaille@Peters-MacBook-Pro.local>
2017-11-30 13:38:01 -0500

    finished formatting of homepage

--- app/javascript/react/src/Peter.js
+++ app/javascript/react/src/Peter.js
@@ -5,13 +5,24 @@ import Resume from './components/Resume'
 import Modal from 'react-modal';
 
 const customStyles = {
+  overlay : {
+    position          : 'fixed',
+    top               : 0,
+    left              : 0,
+    right             : 0,
+    bottom            : 0,
+    backgroundColor   : 'rgba(67, 145, 217, 0.75)'
+  },
   content : {
     top                   : '30%',
     left                  : '50%',
     right                 : 'auto',
     bottom                : 'auto',
-    marginRight           : '-50%',
-    transform             : 'translate(-50%, -50%)'
+    padding                : '40px',
+    // marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)',
+    width                 : '700px'
+
   }
 };
 
@@ -49,44 +60,31 @@ class Peter extends React.Component {
 
            <div className="link-container">
              <div className="link-grid">
-               <a href="https://github.com/pwrobitaille"><div className="fa fa-github" aria-hidden="true"></div></a>
-               <a href="https://www.linkedin.com/in/pwrobitaille/"><div className="fa fa-linkedin" aria-hidden="true"></div></a>
-             </div>
-           </div>
+               <div onClick={this.openModal}>Projects</div>
+               <Modal
+                 isOpen={this.state.modalIsOpen}
+                 onAfterOpen={this.afterOpenModal}
+                 onRequestClose={this.closeModal}
+                 style={customStyles}
+                 contentLabel="Projects"
+                 >
+                 <h2 ref={subtitle => this.subtitle = subtitle}>Projects</h2>
+                  <Projects />
+                 <button onClick={this.closeModal}>close</button>
+               </Modal>
+                 <a href="https://github.com/pwrobitaille"><div className="fa fa-github" aria-hidden="true"></div></a>
+                 <a href="https://www.linkedin.com/in/pwrobitaille/"><div className="fa fa-linkedin" aria-hidden="true"></div></a>
+              <div>Resume</div>
+            </div>
+            {/* </div>
+           </div> */}
+         </div>
 
            <hr className="horizontal-line"></hr>
 
-           <div className="menu-container">
-             <div className="menu-grid">
-               <div onClick={this.openModal}>About</div>
-                 <Modal
-                   isOpen={this.state.modalIsOpen}
-                   onAfterOpen={this.afterOpenModal}
-                   onRequestClose={this.closeModal}
-                   style={customStyles}
-                   contentLabel="Example Modal"
-                  >
-                   <h2 ref={subtitle => this.subtitle = subtitle}>About</h2>
-                    <About />
-                   <button onClick={this.closeModal}>close</button>
-                  </Modal>
-
-                 <div onClick={this.openModal}>Projects</div>
-                 <Modal
-                   isOpen={this.state.modalIsOpen}
-                   onAfterOpen={this.afterOpenModal}
-                   onRequestClose={this.closeModal}
-                   style={customStyles}
-                   contentLabel="Example Modal"
-                 >
-                   <h2 ref={subtitle => this.subtitle = subtitle}>Projects</h2>
-                    <About />
-                   <button onClick={this.closeModal}>close</button>
-                 </Modal>
-
-                <div>Resume</div>
-              </div>
-           </div>
+           <div>
+             <About />
+          </div>
          </div>
     );
   }
