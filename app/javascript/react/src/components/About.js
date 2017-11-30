58082dc0f849e52d694e5b9b693d03d811572263
Peter Robitaille <peterrobitaille@Peters-MacBook-Pro.local>
2017-11-30 16:04:42 -0500

    formatted homepage and added in bio

--- app/javascript/react/src/components/About.js
+++ app/javascript/react/src/components/About.js
@@ -1,12 +1,27 @@
 import React from 'react'
+import headshot from '../../../../../app/assets/images/peter-headshot.jpg'
 
 const About = props => {
   return(
-    <div className="about-container">
-      <div className="about-grid">
-        <h2 className="about">About</h2>
-        <div>Hi I'm Peter Robitaille! Born and raised in the Boston suburbs, I'm a drummer, guitar player, avid concert goer, die hard Patriots, Celtics and Liverpool fan, lover of the outdoors and all things active.  </div>
-        <div>My journey to become a developer began in college. I </div>
+    <div>
+      <div className="about-container">
+        <div className="about-grid">
+          <img src={headshot} alt="" className="pic" />
+          <div className="about-me">
+            <div>Hi I'm Peter Robitaille! Born and raised outside of Boston, I'm a drummer, guitar player, avid concert goer, die hard Patriots, Celtics and Liverpool fan and lover of the outdoors and all things active.</div>
+            {/* <p>When I'm not immersing myself , you can find me </p> */}
+            <p>My journey to become a developer began in college. On a whim, I took an intro to computer science course and was fascinated with the creative and problem solving aspects of programming. Ater graduating college and working in account management for five years, using my knowlege of HTML and CSS to assist my clients, I realized I truly had a passion and interst to learn more. A few online courses later and I was hooked! </p>
+            <p>I decided to join the three month intensive bootcamp <a href="https://launchacademy.com/">Launch Academy</a> and solidify my desire to become a programmer. While at Launch, I established an understanding of full stack development and gained experience with fundamental programming concepts, such as pair programming, collaborative development, test driven development (TDD), and object oriented programming (OOP). I'm beyond excited to put these skills to work as a full stack developer! Feel free to check out my projects and please reach out with any questions</p>
+          </div>
+        </div>
+      </div>
+      <div className="contact">
+        <div className="contact-info">
+          <div>(978) 505-2753</div>
+        </div>
+        <div className="contact-info">
+          <div><a href="mailto:pwrobitaille@gmail.com">pwrobitaille@gmail.com</a></div>
+        </div>
       </div>
     </div>
   )
