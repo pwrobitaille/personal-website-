a35edff15365dc93f5b80275860636ce69a4248e
Peter Robitaille <peterrobitaille@Peters-MacBook-Pro.local>
2017-12-07 12:02:02 -0500

    adjusted text to fit on smaller screens and added additional stylesheets

---
+++ app/javascript/react/src/containers/Skills.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Skills = props => {
+  return (
+    // <div className="skills">
+      <div className="skills-name">{props.skill}</div>
+    /* </div> */
+  )
+}
+  export default Skills
